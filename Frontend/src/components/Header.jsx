c61fce259e117949127f95d1e6ba0955df754348
Sebastian Kaufman <sebastianleonel29@gmail.com>
2025-06-29 22:48:27 -0300

    Agrego carpeta Frontend con App.jsx

--- Frontend/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header style={{ background: "#000", color: "#fff", padding: "1rem 2rem" }}>
-      <h1>Lubricentro Renault</h1>
-    </header>
-  );
-}
--- Frontend/src/pages/Reservar.jsx
+++
@@ -1,8 +0,0 @@
-import FormularioReserva from "../components/FormularioReserva";
-export default function Reservar() {
-  return (
-  <main className="formulario-reserva">
-    <FormularioReserva />
-  </main>    
-  );
-}
