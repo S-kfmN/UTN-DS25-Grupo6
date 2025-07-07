d75b434a684395de328ff151f900a758ca0338bb
Agusgb <147102159+Agusgb@users.noreply.github.com>
2025-07-06 22:39:57 -0300

    Agrego 2 pages
    Contacto.jsx
    BuscarUsuario.jsx

---
+++ Frontend/src/pages/Contacto.jsx
@@ -0,0 +1,30 @@
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
+
+export default function Contacto() {
+  return (
+    <main className="seccion-contenido">
+      <h3>Formulario de Contacto</h3>
+      <div style={{ marginTop: '2rem', marginBottom: '1rem' }}></div>
+      <form className="formulario">
+        <label>Nombre:</label>
+        <input type="text" required />
+
+        <label>Email:</label>
+        <input type="email" required />
+
+        <label>Asunto:</label>
+        <input type="text" required />
+
+        <label>Mensaje:</label>
+        <textarea rows="5" required></textarea>
+
+        <button type="submit" className="boton-principal">Enviar mensaje</button>
+      </form>
+      
+      <h1>Datos de Contacto</h1>
+      <p><b>Dirección:</b> Av. Santa Fe 1860, Buenos Aires, Argentina</p>
+      <p><b>Teléfono:</b> (011) 4813-6052</p>
+      <p><b>Email:</b> contacto@elateneo.com.ar</p>
+    </main>
+  );
+}
